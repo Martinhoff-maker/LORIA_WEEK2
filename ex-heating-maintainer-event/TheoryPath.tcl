69cf54523f60dbdd799a7b25f3f2ac5acb28609a
Zheng Cheng <cz0590@gmail.com>
2020-10-27 15:14:29 +0100

    rebase

---
+++ ex-heating-maintainer-event/TheoryPath.tcl
@@ -0,0 +1,7 @@
+<?xml version="1.0" encoding="UTF-8" standalone="no"?>
+<org.eventb.theory.core.scTheoryLanguageRoot org.eventb.core.accurate="true" org.eventb.core.configuration="org.eventb.theory.core.tul" org.eventb.core.source="/ex-heating-maintainer-event/TheoryPath.tul">
+<org.eventb.theory.core.scAvailableTheoryProject name="'" org.eventb.core.source="/ex-heating-maintainer-event/TheoryPath.tul|org.eventb.theory.core.theoryLanguageRoot#TheoryPath|org.eventb.theory.core.availableTheoryProject#'" org.eventb.theory.core.availableTheoryProject="/theory-axiom-real">
+<org.eventb.theory.core.scAvailableTheory name="THYPH_0" org.eventb.core.source="/ex-heating-maintainer-event/TheoryPath.tul|org.eventb.theory.core.theoryLanguageRoot#TheoryPath|org.eventb.theory.core.availableTheoryProject#'|org.eventb.theory.core.availableTheory#(" org.eventb.theory.core.availableTheory="/theory-axiom-real/Real_ext.dtf|org.eventb.theory.core.deployedTheoryRoot#Real_ext"/>
+<org.eventb.theory.core.scAvailableTheory name="THYPH_1" org.eventb.core.source="/ex-heating-maintainer-event/TheoryPath.tul|org.eventb.theory.core.theoryLanguageRoot#TheoryPath|org.eventb.theory.core.availableTheoryProject#'|org.eventb.theory.core.availableTheory#)" org.eventb.theory.core.availableTheory="/theory-axiom-real/Intervals.dtf|org.eventb.theory.core.deployedTheoryRoot#Intervals"/>
+</org.eventb.theory.core.scAvailableTheoryProject>
+</org.eventb.theory.core.scTheoryLanguageRoot>
